d1e735df6390fc927046ad0def1610993a77c67d
Enzo Ducros <enzoducros@Enzos-MacBook-Air-2.local>
2025-09-24 11:02:12 +0200

    test

--- Homework1.py
+++ Homework1.py
@@ -1,2 +1,29 @@
-a = b 
-A = b+2
+'''owner = "Ducros Enzo"
+date = "2024-09-15"'''
+
+# Part I : Calculating time to save 
+
+def months4house():
+    '''function printing the number of months required to save up for a home depending on user_inputs '''
+    # User input Variables
+    annual_salary = float(input("Enter your annual salary in Lyon: ")) 
+    portion_saved = float(input("Enter the portion of salary to be saved , as a decimal : "))
+    total_cost = float(input("Enter the cost of your dream home in Lyon : "))
+    # Given Variables
+    portion_down_payment = 0.25
+    current_savings = 0.0
+    # output variables
+    nb_months = 0
+    r = 0.04  # investment returns
+    # Start of Program 
+    while current_savings < total_cost*portion_down_payment:
+        if nb_months % 12 == 0:
+            current_savings = current_savings + current_savings*r
+        current_savings += portion_saved*(annual_salary/12)
+        nb_months += 1 
+    print(f"Number of months : {nb_months}")
+
+
+
+if __name__ == "__main__":
+    months4house()
